15b792f3b79a2ef09715bf32268e70f2843632cd
Adam Gignac <adam.gignac@esentire.com>
2019-06-20 12:45:18 -0400

    Reorder urls

--- brain/mapping/urls.py
+++ brain/mapping/urls.py
@@ -22,14 +22,14 @@ from mapping.views import (
 
 urlpatterns = [
     path('', WorkspaceList.as_view(), name='workspace-list'),
-    path('workspaces/new/', CreateWorkspace.as_view(), name='create-workspace'),
-    path('workspaces/<slug>/', WorkspaceDetail.as_view(), name='workspace-detail'),
-    path('workspaces/<slug>/delete/', DeleteWorkspace.as_view(), name='delete-workspace'),
-    path('workspaces/<slug>/add/', CreateItem.as_view(), name='add-item'),
     path('types/', ItemTypeList.as_view(), name='item-type-list'),
     path('types/add/', CreateItemType.as_view(), name='add-item-type'),
     path('types/<slug>/edit', UpdateItemType.as_view(), name='edit-item-type'),
     path('types/<slug>/delete', DeleteItemType.as_view(), name='delete-item-type'),
+    path('workspaces/new/', CreateWorkspace.as_view(), name='create-workspace'),
+    path('workspaces/<slug>/', WorkspaceDetail.as_view(), name='workspace-detail'),
+    path('workspaces/<slug>/delete/', DeleteWorkspace.as_view(), name='delete-workspace'),
+    path('workspaces/<slug>/add/', CreateItem.as_view(), name='add-item'),
     path('workspaces/<workspace>/items/<slug>/', ItemDetail.as_view(), name='item-detail'),
     path('workspaces/<workspace>/items/<slug>/edit', UpdateItem.as_view(), name='update-item'),
     path('workspaces/<workspace>/items/<slug>/delete', DeleteItem.as_view(), name='delete-item'),
